bde83cb4095d156dce86fa338b335d29e15e355b
abdullahboshir <https://github.com/abdullahboshir/t-shirt-react-router>
2022-07-15 11:40:58 +0600

    complete inventory

--- src/Pages/ManageProduct/ManageProduct.js
+++ src/Pages/ManageProduct/ManageProduct.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useServices from '../../hooks/useServices';
+import './manageProduct.css'
 
 const ManageProduct = () => {
+    const [services, setServices] = useServices();
+    const navigate = useNavigate()
     return (
         <div>
-            <h2>This is Manage Products Page </h2>
+          <div className='manage-container'>
+          <h2>Manage Your Inventory</h2>
+            <button className='btn btn-primary'>Add a new item</button>
+            {
+                services.map(service => <div>
+                    <div className="mange">
+                        <div className="manage-item">
+                            <img src={service.img} alt="" />
+                            <h6>{service.name}</h6>
+                            <p>Price: {service.price}</p>
+                            <p>Quantity: {service.quantity}</p>
+                        </div>
+                        <div className="manage-btn">
+                            <button onClick={() => navigate(`/updateStack/${service._id}`)} className='btn btn-info'>INVENTORY</button>
+                            <button  className='btn btn-danger'>DELETE</button>
+
+                        </div>
+                    </div>
+                </div>)
+            }
+          </div>
         </div>
     );
 };
